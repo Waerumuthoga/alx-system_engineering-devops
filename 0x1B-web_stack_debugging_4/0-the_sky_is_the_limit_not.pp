0930ec50484c477c189dceeddfd3893ed12a8a81
Waerumuthoga <waerumuthoga@gmail.com>
2024-05-16 14:45:10 -0700

    task 1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
-# Increases the amount of traffic an Nginx server can handle.
+# Increases the limit of request in the nginx server
 
-# Increase the ULIMIT of the default file
+# Increase the ULIMIT
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
 
-# Restart Nginx
+# Restart the nginx service
 exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
